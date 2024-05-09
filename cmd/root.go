ebcf9bacb7cb493b8da868403a4c94814d962eeb
有点小邪恶 <hengyoush1@163.com>
2024-05-09 23:06:16 +0800

    [eBPF] Add daemon run mode (#4)
     [eBPF] Add daemon mode

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/sevlyar/go-daemon"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -17,7 +18,31 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("run eAPM eBPF Agent ...")
 		log.Printf("collector-addr: %s\n", viper.GetString(common.CollectorAddrVarName))
-		agent.SetupAgent()
+		if viper.GetBool(common.DaemonVarName) {
+			cntxt := &daemon.Context{
+				PidFileName: "./eapm-ebpf.pid",
+				PidFilePerm: 0644,
+				LogFileName: "./eapm-ebpf.log",
+				LogFilePerm: 0640,
+				WorkDir:     "./",
+				// Umask:       027,
+				Args: nil, // use current os args
+			}
+			d, err := cntxt.Reborn()
+			if err != nil {
+				log.Fatal("Unable to run: ", err)
+			}
+			if d != nil {
+				log.Println("eAPM eBPF agent started!")
+				return
+			}
+			defer cntxt.Release()
+			log.Println("----------------------")
+			log.Println("eAPM eBPF agent started!")
+			agent.SetupAgent()
+		} else {
+			agent.SetupAgent()
+		}
 	},
 }
 
@@ -25,12 +50,14 @@ var CollectorAddr string
 var LocalMode bool
 var ConsoleOutput bool
 var Verbose bool
+var Daemon bool
 
 func init() {
 	rootCmd.Flags().StringVar(&CollectorAddr, "collector-addr", "localhost:18800", "backend collector address")
 	rootCmd.Flags().BoolVar(&LocalMode, "local-mode", false, "set true then do not export data to collector")
 	rootCmd.Flags().BoolVarP(&ConsoleOutput, "console-output", "c", true, "print trace data to console")
 	rootCmd.Flags().BoolVarP(&Verbose, "verbose", "v", true, "print verbose log")
+	rootCmd.Flags().BoolVarP(&Daemon, "daemon", "d", false, "run in background")
 	viper.BindPFlags(rootCmd.Flags())
 }
 
