994938fbf20f764844523ed158e040532461194a
hengyoush <hengyoush1@163.com>
2024-09-13 13:11:14 +0800

    [Fix] fix redis nil bug

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"log"
-
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
-)
+import "kyanos/cmd"
 
 func main() {
-	// cmd.Execute()
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
-	}
-	defer ui.Close()
-
-	p := widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
-
-	ui.Render(p)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
-		}
-	}
+	cmd.Execute()
 }
