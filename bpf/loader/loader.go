12da23a6d19eecbfa661b186346f68138e00f973
hengyoush <hengyoush1@163.com>
2024-10-17 02:10:53 +0800

    fix. fix pid filter not works

--- bpf/loader/loader.go
+++ bpf/loader/loader.go
@@ -346,7 +346,10 @@ func setAndValidateParameters(ctx context.Context, options *ac.AgentOptions) boo
 				common.AgentLog.Errorf("Invalid pid : %s\n", each)
 				return false
 			}
-			filterPidMap.Update(pidInt, one, ebpf.UpdateAny)
+			err = filterPidMap.Update(uint32(pidInt), int8(one), ebpf.UpdateAny)
+			if err != nil {
+				common.AgentLog.Errorf("Failed update  FilterPidMap: %s\n", err)
+			}
 		}
 	}
 
