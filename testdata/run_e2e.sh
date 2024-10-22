313fa942949aaf22de9a77a0ed53581c9ffe1077
hengyoush <hengyoush1@163.com>
2024-10-22 11:03:38 +0800

    test: add mysql e2e test

--- testdata/run_e2e.sh
+++ testdata/run_e2e.sh
@@ -46,6 +46,7 @@ function main() {
   bash testdata/test_containerd_filter_by_container_id.sh "$CMD" "$DOCKER_REGISTRY"
   bash testdata/test_containerd_filter_by_container_name.sh "$CMD" "$DOCKER_REGISTRY"
   bash testdata/test_redis.sh "$CMD" "$DOCKER_REGISTRY"
+  bash testdata/test_mysql.sh "$CMD" "$DOCKER_REGISTRY"
   echo "success!"
 }
 
---
+++ testdata/test_mysql.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+. $(dirname "$0")/common.sh
+set -ex
+
+CMD="$1"
+DOCKER_REGISTRY="$2"
+FILE_PREFIX="/tmp/kyanos"
+LNAME="${FILE_PREFIX}_mysql.log"
+
+function test_mysql() {
+    if [ -z "$DOCKER_REGISTRY" ]; then 
+        IMAGE_NAME="mysql:5.7.19"
+    else
+        IMAGE_NAME=$DOCKER_REGISTRY"/library/mysql:5.7.19"
+    fi
+    docker pull "$IMAGE_NAME"
+    mkdir -p /opt/docker_v/mysql/conf
+    pushd /opt/docker_v/mysql/conf
+    touch  my.cnf
+    printf "[mysqld]\nskip_ssl" > my.cnf
+    popd
+
+    cname='test-mysql'
+    docker rm -f $cname || true
+    cid1=$(docker run --name $cname -e MYSQL_ROOT_PASSWORD=123456  -p 3306:3306 -v /opt/docker_v/mysql/conf:/etc/mysql/conf.d -d "$IMAGE_NAME")
+    export cid1
+    echo $cid1
+
+    timeout 30 ${CMD} watch --debug-output mysql --remote-ports 3306 2>&1 | tee "${LNAME}" &
+    sleep 10
+    python3 ./testdata/query_mysql.py
+    wait
+
+    cat "${LNAME}"
+    docker rm -f $cid1 || true
+    cat "${LNAME}" | grep 'SELECT' | grep  'rows = 1'
+    # check_time_detail_completed_with_last_lines "${LNAME}" 1
+    # check_patterns_in_file_with_last_lines "${LNAME}" "Resultset rows = 1" 1
+}
+
+function main() {
+    test_mysql
+}
+
+main
