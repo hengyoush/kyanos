e962a8233488458c25770683192b13efd27c51c3
hengyoush <hengyoush1@163.com>
2024-10-24 20:24:26 +0800

    test: add redis server e2e test

--- testdata/test_redis.sh
+++ testdata/test_redis.sh
@@ -5,9 +5,10 @@ set -ex
 CMD="$1"
 DOCKER_REGISTRY="$2"
 FILE_PREFIX="/tmp/kyanos"
-LNAME="${FILE_PREFIX}_redis.log"
+CLIENT_LNAME="${FILE_PREFIX}_redis_client.log"
+SERVER_LNAME="${FILE_PREFIX}_redis_server.log"
 
-function test_redis() {
+function test_redis_client() {
     if [ -z "$DOCKER_REGISTRY" ]; then 
         IMAGE_NAME="redis:7.0.14"
     else
@@ -21,19 +22,47 @@ function test_redis() {
     export cid1
     echo $cid1
 
-    timeout 30 ${CMD} watch --debug-output redis --remote-ports 6379 2>&1 | tee "${LNAME}" &
+    timeout 30 ${CMD} watch --debug-output redis --remote-ports 6379 2>&1 | tee "${CLIENT_LNAME}" &
     sleep 10
     redis-cli -r 5 hget a key
     wait
 
-    cat "${LNAME}"
+    cat "${CLIENT_LNAME}"
     docker rm -f $cid1 || true
     # check_time_detail_completed_with_last_lines "${LNAME}" 3
-    check_patterns_in_file_with_last_lines "${LNAME}" "HGET" 3
+    check_patterns_in_file_with_last_lines "${CLIENT_LNAME}" "HGET" 3
 }
 
+
+function test_redis_server() {
+    if [ -z "$DOCKER_REGISTRY" ]; then 
+        IMAGE_NAME="redis:7.0.14"
+    else
+        IMAGE_NAME=$DOCKER_REGISTRY"/library/redis:7.0.14"
+    fi
+    docker pull "$IMAGE_NAME"
+
+    cname='test-redis'
+    docker rm -f $cname
+    cid1=$(docker run --name $cname  -p 6379:6379 -d "$IMAGE_NAME")
+    export cid1
+    echo $cid1
+
+    timeout 30 ${CMD} watch --debug-output redis --local-ports 6379 2>&1 | tee "${SERVER_LNAME}" &
+    sleep 10
+    redis-cli -r 5 hget a key
+    wait
+
+    cat "${SERVER_LNAME}"
+    docker rm -f $cid1 || true
+    # check_time_detail_completed_with_last_lines "${LNAME}" 3
+    check_patterns_in_file_with_last_lines "${SERVER_LNAME}" "HGET" 3
+}
+
+
 function main() {
-    test_redis
+    test_redis_client
+    test_redis_server
 }
 
 main
