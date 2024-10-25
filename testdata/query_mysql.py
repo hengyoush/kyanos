b145539a0fff23cd59aa22e4ff0877db7e30258e
hengyoush <hengyoush1@163.com>
2024-10-26 02:20:52 +0800

    test: add mysql server e2e test

--- testdata/query_mysql.py
+++ testdata/query_mysql.py
@@ -1,4 +1,6 @@
 import mysql.connector  
+import time
+import sys
 from mysql.connector import Error  
   
 def create_connection(host_name, user_name, user_password):  
@@ -23,24 +25,29 @@ def create_database(connection, database_name):
     except Error as e:  
         print(f"The error '{e}' occurred")  
   
-def execute_query_in_database(connection, database_name, query):  
+def execute_query_in_database(connection, database_name, query, count):  
     try:  
         cursor = connection.cursor()  
         cursor.execute(f"USE {database_name}")  
-        cursor.execute(query)  
+        for i in range(count):
+            cursor.execute(query)  
+            
+            # If it's a SELECT statement, fetch the results  
+            if query.strip().upper().startswith('SELECT'):  
+                result = cursor.fetchall()  
+                for row in result:  
+                    print(row)  
+            else:  
+                # For other statements like INSERT, UPDATE, DELETE, commit the transaction  
+                connection.commit()  
+                print("Query executed successfully")  
+            # 休眠
+            time.sleep(0.5)
           
-        # If it's a SELECT statement, fetch the results  
-        if query.strip().upper().startswith('SELECT'):  
-            result = cursor.fetchall()  
-            for row in result:  
-                print(row)  
-        else:  
-            # For other statements like INSERT, UPDATE, DELETE, commit the transaction  
-            connection.commit()  
-            print("Query executed successfully")  
     except Error as e:  
         print(f"The error '{e}' occurred")  
   
+count = int(sys.argv[1])
 # Replace with your MySQL server credentials  
 connection = create_connection("localhost", "root", "123456")  
   
@@ -52,16 +59,17 @@ create_database(connection, database_name)
   
 # Example SQL query to execute in the newly created database  
 query = "CREATE TABLE example_table (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100))"  
-execute_query_in_database(connection, database_name, query)  
+execute_query_in_database(connection, database_name, query,1)  
   
 # Another example query to insert data  
 insert_query = "INSERT INTO example_table (name) VALUES ('John Doe')"  
-execute_query_in_database(connection, database_name, insert_query)  
+execute_query_in_database(connection, database_name, insert_query,1)  
   
 # Example SELECT query to retrieve data  
 select_query = "SELECT * FROM example_table"  
-execute_query_in_database(connection, database_name, select_query)  
+execute_query_in_database(connection, database_name, select_query, count) 
   
+time.sleep(1)
 # Close the connection  
 if connection.is_connected():  
     connection.close()  
