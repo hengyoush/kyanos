fb1001478836e282ce1de8c15e9db7fc0522fa8c
spencercjh <spencercjh@gmail.com>
2024-12-31 07:33:47 +0000

    docs: add comments
    Signed-off-by: spencercjh <spencercjh@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -7,13 +7,18 @@ import (
 )
 
 var (
-	Version    string
-	BuildTime  string
+	// Version -X "kyanos/cmd.Version={{.Version}}"
+	Version string
+	// BuildTime -X "kyanos/cmd.CommitID={{.Commit}}"
+	BuildTime string
+	// CommitID -X "kyanos/cmd.BuildTime={{.Date}}"
 	CommitID   string
 	versionCmd = &cobra.Command{
 		Use:   "version",
 		Short: "Print the version of kyanos",
 		Run: func(cmd *cobra.Command, args []string) {
+			// The following vars are set by the linker during build. See the .goreleaser.yaml
+			// reference: https://goreleaser.com/customization/builds/
 			cmd.Println(fmt.Sprintf("Version: %v", Version))
 			cmd.Println(fmt.Sprintf("BuildTime: %v", BuildTime))
 			cmd.Println(fmt.Sprintf("CommitID: %v", CommitID))
