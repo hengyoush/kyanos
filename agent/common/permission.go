10b7bdbdedd0a450a51d008f892cca64a154f0f1
spencercjh <spencercjh@gmail.com>
2025-01-02 09:05:43 +0000

    fix: adapt to e2e test env

--- agent/common/permission.go
+++ agent/common/permission.go
@@ -1,12 +1,17 @@
 package common
 
 import (
+	"os"
 	"slices"
 
 	"github.com/containerd/containerd/pkg/cap"
 )
 
 func HasPermission() (bool, error) {
+	// root is considered as having CAP_BPF capability.
+	if os.Geteuid() == 0 {
+		return true, nil
+	}
 	current, err := cap.Current()
 	if err != nil {
 		return false, err
