4423b8c446f647e39ccf79e121d98e77326b7e71
spencercjh <spencercjh@gmail.com>
2025-01-02 12:24:20 +0000

    fix: DO NOT use containerd cap package

--- agent/common/permission.go
+++ agent/common/permission.go
@@ -1,15 +1,14 @@
 package common
 
 import (
-	"slices"
-
-	"github.com/containerd/containerd/pkg/cap"
+	"golang.org/x/sys/unix"
 )
 
 func HasPermission() (bool, error) {
-	current, err := cap.Current()
-	if err != nil {
+	hdr := unix.CapUserHeader{Version: unix.LINUX_CAPABILITY_VERSION_3}
+	var data [2]unix.CapUserData
+	if err := unix.Capget(&hdr, &data[0]); err != nil {
 		return false, err
 	}
-	return slices.Contains(current, "CAP_BPF"), nil
+	return data[0].Permitted&unix.CAP_BPF != 0, nil
 }
