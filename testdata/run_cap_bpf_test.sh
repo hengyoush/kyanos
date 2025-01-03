ebc5112f91250d02e81d0272a4e9841458e9f3bd
spencercjh <spencercjh@gmail.com>
2025-01-03 08:27:53 +0000

    test: test test_add_cap_bpf first

--- testdata/run_cap_bpf_test.sh
+++ testdata/run_cap_bpf_test.sh
@@ -11,14 +11,14 @@ else
   echo "DOCKER_REGISTRY is missing."
 fi
 
+sudo docker run -d --cap-add CAP_BPF --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+sudo docker cp /host/kyanos/kyanos alpine:/
+sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
+sudo docker exec alpine sh -c 'sh /test_add_cap_bpf.sh "/kyanos"'
+sudo docker stop alpine && sudo docker rm alpine
+
 sudo docker run -d --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_not_add_cap_bpf.sh alpine:/
 sudo docker exec alpine sh -c 'sh /test_not_add_cap_bpf.sh "/kyanos"'
-sudo docker stop alpine && sudo docker rm alpine
-
-sudo docker run -d --add-cap CAP_BPF --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
-sudo docker cp /host/kyanos/kyanos alpine:/
-sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
-sudo docker exec alpine sh -c 'sh /test_add_cap_bpf.sh "/kyanos"'
 sudo docker stop alpine && sudo docker rm alpine
