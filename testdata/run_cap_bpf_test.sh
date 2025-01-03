c480f195d14480a945d0a53a36a694470479cc9c
spencercjh <spencercjh@gmail.com>
2025-01-03 09:23:30 +0000

    test: add missing capability CAP_SYS_RESOURCE

--- testdata/run_cap_bpf_test.sh
+++ testdata/run_cap_bpf_test.sh
@@ -12,16 +12,16 @@ else
 fi
 CAP_TO_ADD="$2"
 
-sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=$CAP_TO_ADD --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
-sudo docker cp /host/kyanos/kyanos alpine:/
-sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
-sudo docker cp /var/lib/kyanos/btf/current.btf alpine:/
-sudo docker exec alpine sh -c 'sh /test_add_cap_bpf.sh "/kyanos --btf /current.btf"'
-sudo docker stop alpine && sudo docker rm alpine
-
-sudo docker run -d --ulimit memlock=100000000000:100000000000 --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=CAP_SYS_RESOURCE --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_not_add_cap_bpf.sh alpine:/
 sudo docker cp /var/lib/kyanos/btf/current.btf alpine:/
 sudo docker exec alpine sh -c 'sh /test_not_add_cap_bpf.sh "/kyanos --btf /current.btf"'
+sudo docker stop alpine && sudo docker rm alpine
+
+sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=$CAP_TO_ADD --cap-add=CAP_SYS_RESOURCE --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+sudo docker cp /host/kyanos/kyanos alpine:/
+sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
+sudo docker cp /var/lib/kyanos/btf/current.btf alpine:/
+sudo docker exec alpine sh -c 'sh /test_add_cap_bpf.sh "/kyanos --btf /current.btf"'
 sudo docker stop alpine && sudo docker rm alpine
