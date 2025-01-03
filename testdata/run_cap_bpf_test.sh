d9ac045498e501a9276789623fae3182794d1196
spencercjh <spencercjh@gmail.com>
2025-01-03 08:50:56 +0000

    test: cap-add difference capability for different kernal

--- testdata/run_cap_bpf_test.sh
+++ testdata/run_cap_bpf_test.sh
@@ -10,14 +10,15 @@ if [ -n "$DOCKER_REGISTRY" ]; then
 else
   echo "DOCKER_REGISTRY is missing."
 fi
+CAP_TO_ADD="$2"
 
-sudo docker run -d --cap-add CAP_BPF --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=$CAP_TO_ADD --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
 sudo docker exec alpine sh -c 'sh /test_add_cap_bpf.sh "/kyanos"'
 sudo docker stop alpine && sudo docker rm alpine
 
-sudo docker run -d --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+sudo docker run -d --ulimit memlock=100000000000:100000000000 --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_not_add_cap_bpf.sh alpine:/
 sudo docker exec alpine sh -c 'sh /test_not_add_cap_bpf.sh "/kyanos"'
