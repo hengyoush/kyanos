97b3013849fe7ed1d4e58a46b4b66c03c8b939f9
spencercjh <spencercjh@gmail.com>
2025-01-03 09:45:16 +0000

    test: try to use --privileged instead of cap-add

--- testdata/run_cap_bpf_test.sh
+++ testdata/run_cap_bpf_test.sh
@@ -10,8 +10,8 @@ if [ -n "$DOCKER_REGISTRY" ]; then
 else
   echo "DOCKER_REGISTRY is missing."
 fi
-CAP_TO_ADD="$2"
 
+# CAP_SYS_RESOURCE reference: https://docs.ebpf.io/linux/concepts/resource-limit/
 sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=CAP_SYS_RESOURCE --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_not_add_cap_bpf.sh alpine:/
@@ -19,7 +19,8 @@ sudo docker cp /var/lib/kyanos/btf/current.btf alpine:/
 sudo docker exec alpine sh -c 'sh /test_not_add_cap_bpf.sh "/kyanos --btf /current.btf"'
 sudo docker stop alpine && sudo docker rm alpine
 
-sudo docker run -d --ulimit memlock=100000000000:100000000000 --cap-add=$CAP_TO_ADD --cap-add=CAP_SYS_RESOURCE --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
+# mount sys reference: https://stackoverflow.com/questions/75808955/error-mounting-sys-kernel-debug-tracing-to-rootfs
+sudo docker run -d -v /sys/:/sys/ --privileged --name alpine $DOCKER_REGISTRY'alpine' sh -c 'sleep 120' || true
 sudo docker cp /host/kyanos/kyanos alpine:/
 sudo docker cp ./testdata/test_add_cap_bpf.sh alpine:/
 sudo docker cp /var/lib/kyanos/btf/current.btf alpine:/
--- testdata/test_add_cap_bpf.sh
+++ testdata/test_add_cap_bpf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -ex
+set -x
 
 CMD="$1"
 FILE_PREFIX="/tmp/kyanos"
